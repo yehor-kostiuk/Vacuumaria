4555a9c1f9d20dac0760ddf87e8b3479fc99e792
Egor Kostiuk <kostukegor729@gmail.com>
2025-09-06 16:19:30 +0300

    feat: add recipe types

--- src/libs/types/types.ts
+++ src/libs/types/types.ts
@@ -6,3 +6,4 @@ export { type Game } from "./game.interface.js";
 export { type Inventory } from "./inventory.interface.js";
 export { type ItemType } from "./item.type.js";
 export { type Item } from "./item.interface.js";
+export { type Recipe } from "./recipe.interface.js";

1bd042e347f3d69410a2c9e7d1259a029cdf5aa1
Egor Kostiuk <kostukegor729@gmail.com>
2025-09-06 13:39:06 +0300

    feat: add game board page

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 
 import "~/assets/css/styles.css";
 
+import { GameBoard } from "~/pages/game-board/game-board.jsx";
+
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>vacuumaria</StrictMode>,
+	<StrictMode>
+		<GameBoard />
+	</StrictMode>,
 );

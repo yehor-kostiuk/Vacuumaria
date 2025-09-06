ab05ac08371ea6d25893997b808094fcdc342b99
Egor Kostiuk <kostukegor729@gmail.com>
2025-09-06 16:52:09 +0300

    feat: add base game store

---
+++ src/libs/modules/store.module.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import type {
+	Inventory,
+	CraftingSlot,
+	CraftingTable,
+	Game,
+	ItemType,
+} from "~/libs/types/types.js";
+
+import { INITIAL_ITEMS, INITIAL_UNLOCKED } from "~/libs/constants/constants.js";
+
+export const initialCraftingTable: CraftingSlot[] = Array.from(
+	{ length: 9 },
+	(_, i) => ({ id: `slot${i + 1}` }),
+);
+
+export const useGameStore = create<Game>(() => ({
+	inventory: { items: INITIAL_ITEMS } as Inventory,
+	craftingTable: initialCraftingTable as CraftingTable,
+	unlockedItems: new Set<ItemType>(INITIAL_UNLOCKED),
+}));

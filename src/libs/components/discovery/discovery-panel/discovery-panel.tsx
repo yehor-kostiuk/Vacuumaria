0849f03f0e7017324370c59b3ccbd43af30e379d
styyyxgl <grozav.n900@gmail.com>
2025-09-06 14:54:26 +0300

    refactor: format the code

--- src/libs/components/discovery/discovery-panel/discovery-panel.tsx
+++ src/libs/components/discovery/discovery-panel/discovery-panel.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import styles from "./discovery-panel.module.css"
+import styles from "./discovery-panel.module.css";
 
 const DiscoveryPanel: React.FC = () => {
-	return (
-		<div className={styles["discovery-panel"]}>
-
-		</div>
-	);
+	return <div className={styles["discovery-panel"]}></div>;
 };
 
 export { DiscoveryPanel };

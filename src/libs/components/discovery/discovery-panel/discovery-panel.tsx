19b47f2cde67d7709d042ec5e08bd2670137e446
styyyxgl <grozav.n900@gmail.com>
2025-09-23 21:01:15 +0300

    refactor: use items from store instead of uniqueItems

--- src/libs/components/discovery/discovery-panel/discovery-panel.tsx
+++ src/libs/components/discovery/discovery-panel/discovery-panel.tsx
@@ -1,18 +1,13 @@
 import styles from "./discovery-panel.module.css";
 import { DiscoveryPanelCard } from "./discovery-panel-card/discovery-panel-card.jsx";
 import { useGameStore } from "~/libs/modules/store.module.js";
-import { type AdvancedItem } from "~/libs/types/types.ts";
 
 const DiscoveryPanel = () => {
 	const items = useGameStore((state) => state.availableForCrafting);
 
-	const uniqueItems: AdvancedItem[] = Array.from(
-		new Map(items.map((item) => [item.item.id, item])).values(),
-	);
-
 	return (
 		<div className={styles["discovery-panel"]}>
-			{uniqueItems.map((item) => (
+			{items.map((item) => (
 				<DiscoveryPanelCard key={item.item.id} item={item} />
 			))}
 		</div>

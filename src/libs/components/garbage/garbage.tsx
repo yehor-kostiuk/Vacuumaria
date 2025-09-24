da039dac2dec70285bc16ca294a59584b52be524
styyyxgl <grozav.n900@gmail.com>
2025-09-24 16:38:12 +0300

    feat: implement clear inventory and reset game actions

--- src/libs/components/garbage/garbage.tsx
+++ src/libs/components/garbage/garbage.tsx
@@ -1,12 +1,40 @@
 import styles from "./garbage.module.css";
+import { useGameStore, initialCraftingTable } from "~/libs/modules/store.module.js";
+import { INITIAL_ITEMS, INITIAL_AVALIABLE_CRAFT_ITEMS } from "~/libs/constants/constants.js";
 
 const Garbage = () => {
+
+	const handleClearInventory = () => {
+		if (window.confirm("Clear inventory?")) {
+			useGameStore.setState({
+				inventory: { items: [] }
+			});
+		}
+	};
+
+	const handleResetGame = () => {
+		if (window.confirm("Reset game?")) {
+			localStorage.removeItem("storage");
+
+			// reset Zustand state
+			useGameStore.setState({
+				inventory: { items: [] },
+				craftingTable: initialCraftingTable,
+				baseItems: INITIAL_ITEMS,
+				unlockedItems: [],
+				availableForCrafting: INITIAL_AVALIABLE_CRAFT_ITEMS,
+			});
+
+			location.reload();
+		}
+	};
+
 	return (
 		<div className={styles["garbage-container"]}>
 			<div className={styles["garbage"]}/>
 			<div className={styles["buttons-container"]}>
-				<button className={styles["clear-button"]} title={"Clear all items from inventory"}>Clear Inventory</button>
-				<button className={styles["reset-button"]} title={"Reset entire game progress"}>Reset Button</button>
+				<button className={styles["clear-button"]} title={"Clear all items from inventory"} onClick={handleClearInventory}>Clear Inventory</button>
+				<button className={styles["reset-button"]} title={"Reset entire game progress"} onClick={handleResetGame}>Reset Button</button>
 			</div>
 		</div>
 	);
